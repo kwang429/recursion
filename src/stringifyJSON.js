f1c1b2aaf58161ae026c9d1b59cbfa84555ff914
Katherine Wang <katherine.wang429@gmail.com>
2019-09-28 13:31:40 -0700

    stringifyJSON stringifies numbers, booleans, strings, and null vals

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,55 @@
 
 // but you don't so you're going to write it from scratch:
 
+// turns an object or value into a string
+
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if (typeof obj === "number") {
+  	return obj.toString();
+
+  } else if (obj === null) {
+  	return "null";
+
+  } else if (typeof obj === "boolean") {
+  	return obj.toString();
+
+  } else if (typeof obj === "string") {
+	return '"' + obj + '"';		// add quotes to each side
+ 
+  } else if (Array.isArray(obj)) {
+	var stringedArray = "";
+
+	if (obj.length === 1) {	// if the array isn't empty
+		return "[" + stringifyJSON(obj[0]) + "]";
+
+	} else if (obj.length > 1) {	// if the array is empty
+		for (var i = 0; i < obj.length; i++) {
+			if (i === 0) {
+				stringedArray = "[" + stringifyJSON(obj[i]) + ",";
+
+			} else if (i === obj.length - 1) {
+				stringedArray += stringifyJSON(obj[i]) + "]";
+
+			} else {
+				stringedArray += stringifyJSON(obj[i]) + ",";
+
+			}
+		}
+		return stringedArray;
+
+  	} else {
+		return "[]";
+	}
+
+  } else if (typeof obj === "object") {
+	  var stringedObj = "";
+
+  	if (Object.keys(obj).length === 0) {
+		return "{}"
+
+	}
+}
+
 };
